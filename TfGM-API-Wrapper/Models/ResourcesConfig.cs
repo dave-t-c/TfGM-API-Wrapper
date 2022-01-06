9c7c23df1c6a8ed86a2c8c7cf8dd13ad224cffad
David Cook <davidthomascook@live.co.uk>
2022-01-06 14:52:39 +0000

    Create a new Resources config POCO
    This will be used for dependency injection for resources.

---
+++ TfGM-API-Wrapper/Models/ResourcesConfig.cs
@@ -0,0 +1,11 @@
+namespace TfGM_API_Wrapper.Models
+{
+    /// <summary>
+    /// POCO for Resources Information.
+    /// This is injected into the StopsController.
+    /// </summary>
+    public class ResourcesConfig
+    {
+        public string StopResourcePath { get; set; }
+    }
+}
