55ddc80fa9c6ac8d55c793481d2506999d9b7e8c
David Cook <davidthomascook@live.co.uk>
2022-01-09 17:25:19 +0000

    Remove the CheckFileRequirements method from StopLoader.
    This is now contained in the LoaderHelper class.

--- TfGM-API-Wrapper/Models/Resources/StopLoader.cs
+++ TfGM-API-Wrapper/Models/Resources/StopLoader.cs
@@ -34,30 +34,6 @@ namespace TfGM_API_Wrapper.Models.Resources
         }
 
         /// <summary>
-        /// Checks the file requirements for a given file in a ResourceConfig
-        /// </summary>
-        /// <param name="filePath">File path to verify</param>
-        /// <param name="argName">Name of the field associated with the file path</param>
-        /// <returns>string - full file path for the resource.</returns>
-        /// <exception cref="InvalidOperationException">
-        /// Thrown when a null file path is given. StopLoaders cannot be created with null resource paths. 
-        /// </exception>
-        /// <exception cref="FileNotFoundException">
-        /// Thrown when the file cannot be found from the base directory.
-        /// </exception>
-        private static string CheckFileRequirements(string filePath, string argName)
-        {
-            if(filePath is null) 
-                throw new InvalidOperationException(argName + " cannot be null");
-            filePath = CurrentDomain.BaseDirectory + filePath;
-            if (!File.Exists(filePath))
-            {
-                throw new FileNotFoundException("Could not find file " + filePath);
-            }
-            return filePath;
-        }
-        
-        /// <summary>
         /// Imports the Stops from the Stops resources file.
         /// The results from this are then returned with a GET to '/api/stops'.
         /// </summary>
