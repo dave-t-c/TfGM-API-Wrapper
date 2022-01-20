deabefe6e3ca85989bfe3a4379172878a92fe12e
David Cook <davidthomascook@live.co.uk>
2022-01-20 16:17:25 +0000

    Add interface for Service Requesters to use.

---
+++ TfGM-API-Wrapper/Models/Services/IRequester.cs
@@ -0,0 +1,11 @@
+namespace TfGM_API_Wrapper.Models.Services
+{
+    /// <summary>
+    /// Specifies the methods and outputs that must be implemented
+    /// for a class to act as a service requester.
+    /// </summary>
+    public interface IRequester
+    {
+        
+    }
+}
