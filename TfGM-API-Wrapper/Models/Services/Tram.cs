aea17283ad9f56f10dce278a3821a622157122ae
David Cook <davidthomascook@live.co.uk>
2022-01-22 20:13:38 +0000

    Modify GetHashCode to combine the destination and carriages values.

--- TfGM-API-Wrapper/Models/Services/Tram.cs
+++ TfGM-API-Wrapper/Models/Services/Tram.cs
@@ -38,7 +38,7 @@ namespace TfGM_API_Wrapper.Models.Services
         //TODO Add HashCode Implementation
         public override int GetHashCode()
         {
-            return Destination.GetHashCode();
+            return HashCode.Combine(Destination, Carriages);
         }
 
     }
