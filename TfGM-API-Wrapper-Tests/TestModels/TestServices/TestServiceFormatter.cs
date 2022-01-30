611ef043b96f2b507ab6e474c225e2f761d7bd8a
David Cook <davidthomascook@live.co.uk>
2022-01-30 20:49:59 +0000

    Add empty ServiceFormatter and test classes.
    These will be used to format retrieved unformatted services.

---
+++ TfGM-API-Wrapper-Tests/TestModels/TestServices/TestServiceFormatter.cs
@@ -0,0 +1,7 @@
+namespace TfGM_API_Wrapper_Tests.TestModels.TestServices
+{
+    public class TestServiceFormatter
+    {
+        
+    }
+}
---
+++ TfGM-API-Wrapper/Models/Services/ServiceFormatter.cs
@@ -0,0 +1,7 @@
+namespace TfGM_API_Wrapper.Models.Services
+{
+    public class ServiceFormatter
+    {
+        
+    }
+}
