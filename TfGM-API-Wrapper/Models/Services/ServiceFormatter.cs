4241255dd9f04ea92aaf21ea25cfdb4f76b725a3
David Cook <davidthomascook@live.co.uk>
2022-01-30 22:04:29 +0000

    Add 'FormatServices' method to ServiceFormatter
    This extracts the trams from the unformatted services object and adds them to the Formatted services.

--- TfGM-API-Wrapper/Models/Services/ServiceFormatter.cs
+++ TfGM-API-Wrapper/Models/Services/ServiceFormatter.cs
@@ -1,7 +1,37 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authentication;
+
 namespace TfGM_API_Wrapper.Models.Services
 {
     public class ServiceFormatter
     {
-        
+
+        public ServiceFormatter()
+        {
+            
+        }
+
+        public FormattedServices FormatServices(List<UnformattedServices> unformattedServices)
+        {
+            FormattedServices formattedServices = new FormattedServices();
+            foreach (UnformattedServices service in unformattedServices)
+            {
+                Tram tram = new Tram(service.Dest0, service.Carriages0, service.Status0,
+                    service.Wait0);
+                formattedServices.AddService(tram);
+                
+                tram = new Tram(service.Dest1, service.Carriages1, service.Status1,
+                    service.Wait1);
+                formattedServices.AddService(tram);
+                
+                tram = new Tram(service.Dest2, service.Carriages2, service.Status2,
+                    service.Wait2);
+                formattedServices.AddService(tram);
+                
+                formattedServices.AddMessage(service.MessageBoard);
+            }
+            
+            return formattedServices;
+        }
     }
 }
