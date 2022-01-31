5cfb84be6557df2d21a905138bcf3ad776c23a84
David Cook <davidthomascook@live.co.uk>
2022-01-31 22:04:29 +0000

    Modify FormatServices to use a new AddTram method.
    This checks if the destination property is null or empty, and does not add the tram if so.

--- TfGM-API-Wrapper/Models/Services/ServiceFormatter.cs
+++ TfGM-API-Wrapper/Models/Services/ServiceFormatter.cs
@@ -11,27 +11,43 @@ namespace TfGM_API_Wrapper.Models.Services
             
         }
 
+        /// <summary>
+        /// Converts a list of unformatted services into a Formatted services object.
+        /// </summary>
+        /// <param name="unformattedServices">List of Unformatted services to format.</param>
+        /// <returns>Formatted Services: Combined result of each of the unformatted services.</returns>
         public FormattedServices FormatServices(List<UnformattedServices> unformattedServices)
         {
             FormattedServices formattedServices = new FormattedServices();
             foreach (UnformattedServices service in unformattedServices)
             {
-                Tram tram = new Tram(service.Dest0, service.Carriages0, service.Status0,
-                    service.Wait0);
-                formattedServices.AddService(tram);
-                
-                tram = new Tram(service.Dest1, service.Carriages1, service.Status1,
-                    service.Wait1);
-                formattedServices.AddService(tram);
-                
-                tram = new Tram(service.Dest2, service.Carriages2, service.Status2,
-                    service.Wait2);
-                formattedServices.AddService(tram);
+                // This currently appears to be the easiest way to create information for all four possible trams.
+                AddTram(formattedServices, new Tram(service.Dest0, service.Carriages0, service.Status0,
+                    service.Wait0));
+                AddTram(formattedServices, new Tram(service.Dest1, service.Carriages1, service.Status1,
+                    service.Wait1));
+                AddTram(formattedServices, new Tram(service.Dest2, service.Carriages2, service.Status2,
+                    service.Wait2));
+                AddTram(formattedServices, new Tram(service.Dest3, service.Carriages3, service.Status3,
+                    service.Wait3));
                 
                 formattedServices.AddMessage(service.MessageBoard);
             }
             
             return formattedServices;
+        }
+
+        /// <summary>
+        /// Adds a tram to the formatted services if the destination string is not null or empty.
+        /// This assumes that if the destination is null or empty, the other fields such as carriages will
+        /// also be null or empty.
+        /// </summary>
+        /// <param name="formattedServices">Formatted service to add tram to</param>
+        /// <param name="tram">Tram to add to formatted services</param>
+        private static void AddTram(FormattedServices formattedServices, Tram tram)
+        {
+            if (string.IsNullOrEmpty(tram.Destination)) return;
+            formattedServices.AddService(tram);
         }
     }
 }
