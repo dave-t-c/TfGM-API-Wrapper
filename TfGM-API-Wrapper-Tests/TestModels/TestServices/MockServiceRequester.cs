23f95b05618b7130f7cd1771c730a282788901e8
David Cook <davidthomascook@live.co.uk>
2022-02-04 23:17:53 +0000

    Add RequestServices method to MockServiceRequester.
    This is not currently implemented.

--- TfGM-API-Wrapper-Tests/TestModels/TestServices/MockServiceRequester.cs
+++ TfGM-API-Wrapper-Tests/TestModels/TestServices/MockServiceRequester.cs
@@ -1,9 +1,13 @@
+using System.Collections.Generic;
 using TfGM_API_Wrapper.Models.Services;
 
 namespace TfGM_API_Wrapper_Tests.TestModels.TestServices
 {
     public class MockServiceRequester: IRequester
     {
-        
+        public List<UnformattedServices> RequestServices(List<int> ids)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
