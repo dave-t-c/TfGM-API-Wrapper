bbe4eb3cf3dff9f0adb07221f040006c7e34be6a
David Cook <davidthomascook@live.co.uk>
2022-02-05 09:58:53 +0000

    Implement RequestServices in ServiceProcessor.
    This looks up the ids for the stop, then uses the requester obj given to get the services, and uses the service formatter to format the results.

--- TfGM-API-Wrapper/Models/Services/ServiceProcessor.cs
+++ TfGM-API-Wrapper/Models/Services/ServiceProcessor.cs
@@ -1,10 +1,34 @@
+using System.Collections.Generic;
+using TfGM_API_Wrapper.Models.Resources;
+using TfGM_API_Wrapper.Models.Stops;
+
 namespace TfGM_API_Wrapper.Models.Services
 {
     public class ServiceProcessor
     {
-        public ServiceProcessor(IRequester requester)
+        private IRequester _requester;
+        private ImportedResources _resources;
+        private StopLookup _stopLookup;
+        private ServiceFormatter _serviceFormatter;
+        
+        public ServiceProcessor(IRequester requester, ImportedResources resources)
         {
-            
+            _requester = requester;
+            _resources = resources;
+            _stopLookup = new StopLookup(resources);
+            _serviceFormatter = new ServiceFormatter();
+        }
+
+        /// <summary>
+        /// Returns services for a given stop name or Tlaref. 
+        /// </summary>
+        /// <param name="stop">Stop Id, either name or Tlaref. </param>
+        /// <returns>Formatted Services for given stop</returns>
+        public FormattedServices RequestServices(string stop)
+        {
+            List<int> stopIds = _stopLookup.LookupIDs(stop);
+            List<UnformattedServices> unformattedServicesList = _requester.RequestServices(stopIds);
+            return _serviceFormatter.FormatServices(unformattedServicesList);
         }
     }
 }
