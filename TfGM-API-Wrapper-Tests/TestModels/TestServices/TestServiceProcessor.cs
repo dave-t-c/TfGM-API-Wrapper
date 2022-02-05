b0544e86688f4abbce8b0b37c2f01b8a2adb2395
David Cook <davidthomascook@live.co.uk>
2022-02-05 09:55:39 +0000

    Add test to use the service procesor to generate and format the services.
    This uses the MockServiceRequester for imitating service requests.

--- TfGM-API-Wrapper-Tests/TestModels/TestServices/TestServiceProcessor.cs
+++ TfGM-API-Wrapper-Tests/TestModels/TestServices/TestServiceProcessor.cs
@@ -1,4 +1,7 @@
+using System.Diagnostics;
 using NUnit.Framework;
+using TfGM_API_Wrapper.Models.Resources;
+using TfGM_API_Wrapper.Models.Services;
 
 namespace TfGM_API_Wrapper_Tests.TestModels.TestServices
 {
@@ -8,16 +11,53 @@ namespace TfGM_API_Wrapper_Tests.TestModels.TestServices
     public class TestServiceProcessor
     {
         private MockServiceRequester? _mockServiceRequester;
+        private ServiceProcessor? _serviceProcessor;
+        private ResourcesConfig? _validResourcesConfig;
+        private ImportedResources? _importedResources;
+        
+        private ResourceLoader? _resourceLoader;
+        private const string StopResourcePathConst = "../../../Resources/ValidStopLoader.json";
+        private const string StationNamesToTlarefsPath = "../../../Resources/Station_Names_to_TLAREFs.json";
+        private const string TlarefsToIdsPath = "../../../Resources/TLAREFs_to_IDs.json";
+        
         [SetUp]
         public void SetUp()
         {
+            _validResourcesConfig = new ResourcesConfig
+            {
+                StopResourcePath = StopResourcePathConst,
+                StationNamesToTlarefsPath = StationNamesToTlarefsPath,
+                TlarefsToIdsPath = TlarefsToIdsPath
+            };
+
+            _resourceLoader = new ResourceLoader(_validResourcesConfig);
+            _importedResources = _resourceLoader.ImportResources();
+
             _mockServiceRequester = new MockServiceRequester();
+            _serviceProcessor = new ServiceProcessor(_mockServiceRequester, _importedResources);
+
         }
 
         [TearDown]
         public void TearDown()
         {
             _mockServiceRequester = null;
+        }
+
+
+        /// <summary>
+        /// This should take a stop name, and then return the expected formatted services.
+        /// This will use tge MockServiceRequester so we can test an expected value as the data
+        /// will be static.
+        /// This should return FormattedServices.
+        /// </summary>
+        [Test]
+        public void TestProcessServicesStopName()
+        {
+            Debug.Assert(_serviceProcessor != null, nameof(_serviceProcessor) + " != null");
+            FormattedServices result = _serviceProcessor.RequestServices("BMR");
+            Assert.NotNull(result);
+            Assert.AreEqual(1, result.Destinations.Count);
         }
     }
 }
