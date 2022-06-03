145a74e4f9a5512cb59f28c414109dc03a494c32
David Cook <davidthomascook@live.co.uk>
2022-06-03 17:04:29 +0100

    Fix doc warnings in Program.cs

--- TfGM-API-Wrapper/Program.cs
+++ TfGM-API-Wrapper/Program.cs
@@ -3,8 +3,15 @@ using Microsoft.Extensions.Hosting;
 
 namespace TfGM_API_Wrapper;
 
+/// <summary>
+/// Configures Host Builder and acts as program entry point
+/// </summary>
 public static class Program
 {
+    /// <summary>
+    /// Program entry point
+    /// </summary>
+    /// <param name="args">Command line args. These are not used</param>
     public static void Main(string[] args)
     {
         CreateHostBuilder(args).Build().Run();
