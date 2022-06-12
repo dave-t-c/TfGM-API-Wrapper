b80a24756df4c9c8795fd6381906fba659a0de28
David Cook <davidthomascook@live.co.uk>
2022-06-12 19:57:38 +0100

    Add doc comments to services data model.

--- TfGM-API-Wrapper/Models/Services/ServicesDataModel.cs
+++ TfGM-API-Wrapper/Models/Services/ServicesDataModel.cs
@@ -2,11 +2,19 @@ using TfGM_API_Wrapper.Models.Resources;
 
 namespace TfGM_API_Wrapper.Models.Services;
 
+/// <summary>
+/// Data model that handles requests for service information.
+/// </summary>
 public class ServicesDataModel: IServicesDataModel
 {
     
     private readonly ServiceProcessor _serviceProcessor;
 
+    /// <summary>
+    /// Creates a new Services model using provided resources and requester
+    /// </summary>
+    /// <param name="importedResources">Imported resources to be used for stops infromation</param>
+    /// <param name="requester">Requester responsible for live service requests.</param>
     public ServicesDataModel(ImportedResources importedResources, IRequester requester)
     {
         _serviceProcessor = new ServiceProcessor(requester, importedResources);
