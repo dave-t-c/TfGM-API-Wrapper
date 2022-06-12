9b0b1c3220ebebaeefedbc183c19b0d788506785
David Cook <davidthomascook@live.co.uk>
2022-06-12 17:10:18 +0100

    Add TestServicesDataModel including a test to request services for BMR.
    This should return the expected 1 destionation as the mock service requester is used.

---
+++ TfGM-API-Wrapper-Tests/TestModels/TestServices/TestServicesDataModel.cs
@@ -0,0 +1,50 @@
+using System.Diagnostics;
+using NUnit.Framework;
+using TfGM_API_Wrapper.Models.Resources;
+using TfGM_API_Wrapper.Models.Services;
+
+namespace TfGM_API_Wrapper_Tests.TestModels.TestServices;
+
+public class TestServicesDataModel
+{
+    private ResourcesConfig? _resourcesConfig;
+    private ImportedResources? _importedResources;
+    private IRequester? _requester;
+    private ServicesDataModel? _servicesDataModel;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _resourcesConfig = new ResourcesConfig
+        {
+            StopResourcePath = "../../../Resources/ValidStopLoader.json",
+            StationNamesToTlarefsPath = "../../../Resources/Station_Names_to_TLAREFs.json",
+            TlarefsToIdsPath = "../../../Resources/TLAREFs_to_IDs.json"
+        };
+        _importedResources = new ResourceLoader(_resourcesConfig).ImportResources();
+        _requester = new MockServiceRequester();
+        _servicesDataModel = new ServicesDataModel(_importedResources, _requester);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _resourcesConfig = null;
+        _importedResources = null;
+        _requester = null;
+        _servicesDataModel = null;
+    }
+
+    /// <summary>
+    /// Request services for 'BMR".
+    /// This is one of the values returned in the mock service requester, with 1 destination expected.
+    /// </summary>
+    [Test]
+    public void TestRequestExpectedService()
+    {
+        var result = _servicesDataModel?.RequestServices("BMR");
+        Assert.NotNull(result);
+        Debug.Assert(result != null, nameof(result) + " != null");
+        Assert.AreEqual(1, result.Destinations.Count);
+    }
+}
